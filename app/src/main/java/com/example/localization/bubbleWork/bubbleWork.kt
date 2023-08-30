1f2a40500db60b5a1f069e8973c5698eea100eac
Marlos <marlostrinidad@gmail.com>
2023-08-30 16:48:30 -0400

    funcionando loc 6

--- app/src/main/java/com/example/localization/bubbleWork/bubbleWork.kt
+++ app/src/main/java/com/example/localization/bubbleWork/bubbleWork.kt
@@ -3,34 +3,39 @@ package com.example.localization.bubbleWork
 import android.content.Context
 import android.content.Intent
 import android.graphics.PixelFormat
+import android.hardware.display.DisplayManager
 import android.os.Build
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.Display
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
-import android.view.ViewGroup
 import android.view.WindowManager
-import android.widget.FrameLayout
 import android.widget.ImageView
-import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.isVisible
 import com.example.localization.MainActivity
 import com.example.localization.R
+import java.util.Calendar
+
 
 class BubbleWork {
     private lateinit var windowManager: WindowManager
     private lateinit var floatingBubble: View
-    private lateinit var floatingClosed: View
     private var initialX: Int = 0
     private var initialY: Int = 0
+    private var height:Int=0
+    private var width:Int=0
     private var initialTouchX: Float = 0.toFloat()
     private var initialTouchY: Float = 0.toFloat()
     private lateinit var params : WindowManager.LayoutParams
-    private lateinit var paramsClosed : WindowManager.LayoutParams
+    private lateinit var imageparams  : WindowManager.LayoutParams
     private lateinit var inflater:LayoutInflater
     private lateinit var context: Context
-
-
+    private lateinit var imageViewClose:ImageView
+    private  var maxClickDuration:Int = 100
+    private  var  startclickTime:Long = 0
 
     fun Start(appcontext: Context) {
          context = appcontext
@@ -38,13 +43,14 @@ class BubbleWork {
         loadFloatingBubble(inflater)
     }
 
-    private fun openApp(event: MotionEvent): Boolean {
+    private fun upBubble(event: MotionEvent): Boolean {
         val diffPosicaoX = (event.rawX - initialTouchX).toInt()
         val diffPosicaoY = (event.rawY - initialTouchY).toInt()
 
-        val singleClick: Boolean = diffPosicaoX < 5 && diffPosicaoY < 5
+        val clickDuration: Long = Calendar.getInstance().timeInMillis - startclickTime
+        imageViewClose.visibility = View.INVISIBLE
 
-        if (singleClick) {
+        if (clickDuration<maxClickDuration) {
             val intent = Intent(context, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             context.startActivity(intent)
@@ -57,14 +63,31 @@ class BubbleWork {
         params.y = initialY + (event.rawY - initialTouchY).toInt()
 
         windowManager.updateViewLayout(floatingBubble, params)
+
+
+        if(params.y>(height*0.6)){
+            imageViewClose.setImageResource(R.drawable.cancel_orange)
+            if(params.y>(height*0.8)){
+                if (floatingBubble.isVisible){
+                    windowManager.removeView(floatingBubble)
+                    windowManager.removeView(imageViewClose)
+                }
+            }
+
+        }else{
+            imageViewClose.setImageResource(R.drawable.cancel_white)
+        }
         return true
     }
 
-    private fun storeTouchs(event: MotionEvent): Boolean {
+    private fun downBubble(event: MotionEvent): Boolean {
+
+        startclickTime= Calendar.getInstance().timeInMillis
         initialX = params.x
         initialY = params.y
         initialTouchX = (event.rawX)
         initialTouchY = (event.rawY)
+        imageViewClose.visibility = View.VISIBLE
         return true
     }
 
@@ -72,7 +95,6 @@ class BubbleWork {
     private fun loadFloatingBubble(inflater: LayoutInflater){
 
         floatingBubble = inflater.inflate(R.layout.bubble_widget_layout,null)
-        floatingClosed = inflater.inflate(R.layout.bubble_widget_closed,null)
 
         if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O) {
             params = WindowManager.LayoutParams(
@@ -82,56 +104,59 @@ class BubbleWork {
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT
             )
-            paramsClosed = WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
+
+             imageparams = WindowManager.LayoutParams(
+                140,
+                140,
                 WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT
             )
-        }else{
-            params = WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT)
-            paramsClosed = WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT)
         }
-        params.gravity =Gravity.TOP or Gravity.START
-        paramsClosed.gravity =Gravity.BOTTOM or Gravity.BOTTOM
-        params.x = 0
-        paramsClosed.x = 0
-        params.y = 100
-        paramsClosed.y = 100
+             ///icon
+            params.gravity = Gravity.TOP or Gravity.START
+            params.x = 0
+            params.y = 100
+
+             ///close
+           imageparams.gravity = Gravity.BOTTOM or Gravity.CENTER
+           imageparams.y = 100
+
+        imageViewClose= ImageView(context)
+        imageViewClose.setImageResource(R.drawable.cancel_white)
+        imageViewClose.visibility = View.INVISIBLE
+
 
         windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
         windowManager.addView(floatingBubble, params)
-        windowManager.addView(floatingClosed, paramsClosed)
+        windowManager.addView(imageViewClose, imageparams)
 
-        val closeButtonCollapsed = floatingBubble.findViewById<ImageView>(R.id.close_btn)
-        closeButtonCollapsed.setOnClickListener {
-            if (floatingBubble.isVisible) windowManager.removeView(floatingBubble)
-        }
+        height=getDisplayMetrics(context).heightPixels
+        width =getDisplayMetrics(context).widthPixels
+        Log.d("MYTAG",height.toString())
+        Log.d("MYTAG",width.toString())
+
 
         val bubbleView: View = floatingBubble.findViewById(R.id.bolha_layout)
         bubbleView.setOnTouchListener { view, event ->
             view.performClick()
             when (event.action) {
-                MotionEvent.ACTION_DOWN ->  storeTouchs(event)
+                MotionEvent.ACTION_DOWN ->  downBubble(event)
 
                 MotionEvent.ACTION_MOVE ->  moveBubble(event)
 
-                MotionEvent.ACTION_UP   ->  openApp(event)
+                MotionEvent.ACTION_UP   ->  upBubble(event)
 
                 else ->  false
             }
         }
+    }
+
+    private fun getDisplayMetrics(context: Context): DisplayMetrics {
+        val displayManager = context.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+        val defaultDisplay = displayManager.getDisplay(Display.DEFAULT_DISPLAY)
+        val defaultDisplayContext = context.createDisplayContext(defaultDisplay)
+        return defaultDisplayContext.resources.displayMetrics
     }
 
 
